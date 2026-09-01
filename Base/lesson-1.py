779d10f5d94e9db632f794a96cb73b26daf8f75e
User <dimashavrov24@gmail.com>
2026-09-01 19:13:56 +0200

    понятие переменной

---
+++ Base/lesson-1.py
@@ -0,0 +1,11 @@
+# Переменная
+num = 16
+# = - оператор присваивания
+
+# Нотации -правило оформления названий переменных
+# snake_case - client_age, age_of_client
+# camelCase - clientAge, ageOfClient
+# PascalCase - ClientAge, AgeOfClient
+
+# Правила алфавита для названия переменных
+# Можно использовать: a-z, A-Z, 0-9, _
