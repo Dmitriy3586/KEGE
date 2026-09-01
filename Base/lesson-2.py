a364db491fd244e07b09e2d74d8ef2c8746fe9e7
User <dimashavrov24@gmail.com>
2026-09-01 19:14:25 +0200

    типы данных

---
+++ Base/lesson-2.py
@@ -0,0 +1,28 @@
+# Типы данных
+
+# Целое числа / Intenger / int
+my_int = 5
+print(type(my_int))
+
+#Дробное, вещественное, с плавающей точкой число / Float / float
+my_float =4.7
+print(type(my_float))
+
+# Строка / String / str
+my_str_1 = "Hello"
+my_str_2 = 'World'
+print(type(my_str_1))
+
+# Примеры сложения переменных
+# print(my_str_1 + my_str_2) - две строки ОК
+# print(my_int + my_float) - два числа ОК
+# print(my_str_1 + my_int) - сторка и чсило, ошибка конкатенации (объединение строк)
+# print(my_int + my_str_1) - число и сторка, ошибка сложения
+
+# Список / List / list
+my_list = ['Vladimir',18,140.5]
+print(type(my_list))
+
+# Кортеж / Tuple / tuple
+my_tuple =('Vladimir',18,140.5)
+print(type(my_tuple))
