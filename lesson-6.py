20def073c8bc1174f19c99e54386423b2ba112a6
User <dimashavrov24@gmail.com>
2026-09-02 21:55:34 +0200

    модули и библиотеки

---
+++ lesson-6.py
@@ -0,0 +1,30 @@
+# Модули и библиотеки
+
+# Импорт всей библиотеки
+import math
+
+# Импорт всей бибилиоткеки через псевдоним
+import math as m
+
+# Импорт одной команды
+from math import floor
+
+# Импорт одной команды через псевдоним
+from math import floor as f
+
+# Импорт нескольких команд
+from math import log2, log10
+
+# Импорт нескольких команды через псевдоним
+from math import log2 as l2, log10 as l10
+
+# Импорт всех команд
+from math import *
+
+num = 10.5
+print(math.ceil(num)) # округление в большую сторону
+print(floor(num)) # округление в меньшкю сторону
+print(log2(16))
+print(log10(100))
+
+
